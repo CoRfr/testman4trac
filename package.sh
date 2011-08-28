21898479536551faf53744bdf7c5224d032349f5
seccanj <seccanj@48c35240-c592-4893-b96b-4116bf53a44b>
2011-08-28 19:44:41 +0000

    Updated package.sh to do the full job of building archives and publishing on SF and BB.
    git-svn-id: http://svn.code.sf.net/p/testman4trac/code@34 48c35240-c592-4893-b96b-4116bf53a44b

--- package.sh
+++ package.sh
@@ -1,6 +1,12 @@
 VER=$1
 
-zip -r testman4trac.$VER.zip bin
+. ./clean.sh
+. ./build.sh
+
+zip -r ../testman4trac.$VER.zip bin
+tar cvzf ../testmanager.$VER.tar.gz bin
+
+. ./clean.sh
 
 mkdir testman4trac.$VER
 
@@ -16,9 +22,32 @@ cd testman4trac.$VER
 
 . ./clean.sh
 find . -type d -name .svn -print -exec rm -rf {} \;
+find . -type d -name .hg -print -exec rm -rf {} \;
+find . -type f -name "*.bak" -print -exec rm -f {} \;
 
 cd ..
 
-zip -r testman4trac.$VER.src.zip testman4trac.$VER
+zip -r ../testman4trac.$VER.src.zip testman4trac.$VER
+tar cvzf ../testmanager.$VER.src.tar.gz testman4trac.$VER
 
-rm -rf testman4trac.$VER
+svn co https://testman4trac.svn.sourceforge.net/svnroot/testman4trac testman4trac.$VER.SVN
+cd testman4trac.$VER.SVN
+cp -R ../testman4trac.$VER/* .
+svn status
+svn add
+svn commit
+
+cd ..
+
+hg clone https://seccanj@bitbucket.org/olemis/testman4trac testman4trac.$VER.BITBKT
+cd testman4trac.$VER.BITBKT
+cp -R ../testman4trac.$VER/* .
+hg status
+hg add
+hg commit
+hg push
+
+cd ..
+#rm -rf testman4trac.$VER testman4trac.$VER.SVN testman4trac.$VER.BITBKT
+
+
